268ab1f6aee14bee948ec6608b383b571f763281
unknown <qj_joy@yahoo.com>
2023-03-18 01:11:06 -0400

    added MianHeader, MainNavigation, and NanLink components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNaviagtion from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return(
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users/>
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace/>
-        </Route>
-        <Redirect to="/"/>
-      </Switch>
+      <MainNaviagtion/>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users/>
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace/>
+          </Route>
+          <Redirect to="/"/>
+        </Switch>
+      </main>
     </Router>
   );
 }
